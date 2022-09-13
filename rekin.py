fb6a7522a4805de68d12531e2a125761ebf5b0ff
AliksBogdanovs <90691932+AliksBogdanovs@users.noreply.github.com>
2022-09-13 12:56:43 +0300

    Create rekin.py

---
+++ rekin.py
@@ -0,0 +1,25 @@
+class Rekins():
+    def __init__(self,klients,veltijums,izmers,materials):
+        self.klients = klients
+        self.veltijums = veltijums
+        self.izmers = izmers
+        self.materials = materials
+
+    def izdrukat(self):
+        print("Klients:",self.klients)
+        print("Veltījums",self.veltijums)
+        print("Izmērs",self.izmers)
+        print("Materiāls",self.materials)
+
+    def aprekins():
+    darba_samaksa = 15
+    PVN = 21
+    produkta_cena = (veltījuma teksta garums simbolos) * 1.2 + (platums/100 * garums/100 * augstums/100)/ 3 * materiala_cena 
+    PVN_summa = (produkta_cena + darba_samaksa)*PVN/100 
+    rekina_summa = (produkta_cena + darba_samaksa) + PVN_summa
+
+ 
+        
+
+pirmais = Rekins("Anna","Lai izdodas!",[20,20,20],3.5)
+pirmais.izdrukat()
